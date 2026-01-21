85d0e67925e50b0e8d07f41672c270801e34eaac
João Pedro <joaopedrodev@hotmail.com>
2026-01-21 12:07:28 -0300

    environment setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <span>Notion Clone</span>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  );
+}
