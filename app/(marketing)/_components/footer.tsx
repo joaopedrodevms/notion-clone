3c1de604f14671b7d8611e412c9f8d39cdd80f7b
João Pedro <joaopedrodev@hotmail.com>
2026-01-29 11:30:27 -0300

    refactor(layouts): remove dark background classes from main and marketing layouts

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import Logo from "./logo";
 
 const Footer = () => {
     return (
-        <div className="flex items-center w-full p-6 dark:bg-[#1F1F1F] z-50">
+        <div className="flex items-center w-full p-6  z-50">
             <Logo />
             <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
                 <Button variant="ghost" size="sm">
--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -10,8 +10,8 @@ const font = Poppins({
 const Logo = () => {
     return (
         <div className="hidden md:flex items-center gap-x-2">
-            <Image src={"/notion-dark.png"} alt="Logo" width={30} height={30} className="dark:hidden"/>
-            <Image src={"/notion-light.png"} alt="Logo" width={30} height={30} className="hidden dark:block"/>
+            <Image src={"/notion-light.png"} alt="Logo" width={30} height={30} className="dark:hidden"/>
+            <Image src={"/notion-dark.png"} alt="Logo" width={30} height={30} className="hidden dark:block"/>
             <p className={cn("font-bold", font.className)}>Knowledge</p>
         </div>
     );
