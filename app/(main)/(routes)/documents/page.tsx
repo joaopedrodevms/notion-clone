8c7b3eeddfd2a7a2b792bca89321e3e50bb1401d
João Pedro <joaopedrodev@hotmail.com>
2026-01-28 22:51:59 -0300

    feat(documents): create empty page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusIcon } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
+
+    const { user } = useUser();
+
     return (
-        <div>
-            <h1>Documents</h1>
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+                src={"/empty-light.png"}
+                alt="Empty"
+                width={300}
+                height={300}
+                className="dark:hidden"
+            />
+            <Image
+                src={"/empty-dark.png"}
+                alt="Empty"
+                width={300}
+                height={300}
+                className="hidden dark:block"
+            />
+            <h2 className="text-lg font-medium">
+                Olá {user?.firstName}! Bem vindo ao Knowledge
+            </h2>
+            <Button>
+                <PlusIcon className="w-4 h-4" />
+                Novo Documento
+            </Button>
         </div>
     );
 };
