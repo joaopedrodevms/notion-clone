7480ec886c9e569220de7747a68ab47a5ff825d6
João Pedro <joaopedrodev@hotmail.com>
2026-02-02 23:11:00 -0300

    feat: add document list

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -2,9 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { Kbd } from "@/components/ui/kbd";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDownIcon, ChevronRightIcon, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDownIcon, ChevronRightIcon, LucideIcon, PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
     id?: Id<"documents">;
@@ -32,22 +37,40 @@ export const Item = ({
     icon: Icon
 }: ItemProps) => {
 
+    const router = useRouter();
+    const create = useMutation(api.documents.create);
+
+    const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        onExpand?.();
+    }
+
+    const onCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        if (!id) return;
+
+        const promise = create({
+            title: "Untitled",
+            parentDocument: id
+        })
+        .then((documentId) => {
+            if (!expanded) {
+                onExpand?.();
+            }
+            // router.push(`/documents/${documentId}`);
+        });
+
+        toast.promise(promise, {
+            loading: "Criando documento...",
+            success: "Documento criado com sucesso!",
+            error: "Erro ao criar documento!",
+        });
+    }
 
     const ChevronIcon = expanded ? ChevronDownIcon : ChevronRightIcon;
 
 
     return (
-        // <div
-        //     onClick={onClick}
-        //     role="button"
-        //     style={{ paddingLeft: "12px" }}
-        //     className="group min-h-[27px] text-sm py- 1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
-        // >
-        //     <Icon className="shrink-0 size-5 mr-2 text-muted-foreground" />
-        //     <span className="truncate">
-        //         {label}
-        //     </span>
-        // </div>
         <Button
             onClick={onClick}
             variant={"ghost"}
@@ -60,7 +83,7 @@ export const Item = ({
             )}
         >
             {!!id && (
-                <div onClick={() => { }}>
+                <div onClick={handleExpand}>
                     <ChevronIcon className="size-4 shrink-0 text-muted-foreground" />
                 </div>
             )}
@@ -77,6 +100,27 @@ export const Item = ({
             {isSearch && (
                 <Kbd className="ml-auto">Ctrl + K</Kbd>
             )}
+            {!!id && (
+                <div className="ml-auto flex items-center gap-x-2">
+                    <div role="button" onClick={onCreate} className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                        <PlusIcon className="size-4 text-muted-foreground"/>
+                    </div>
+                </div>
+            )}
         </Button>
+    )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+    return (
+        <div
+            style={{
+                paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+            }}
+            className="flex gap-2 py-[3px]"
+        >
+            <Skeleton className="size-4"/>
+            <Skeleton className="h-4 w-[30%]"/>
+        </div>
     )
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -11,12 +11,12 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 const Navigation = () => {
 
     const pathName = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
@@ -147,9 +147,7 @@ const Navigation = () => {
                     />
                 </div>
                 <div className="mt-4">
-                    {documents?.map(document => (
-                        <p key={document._id}>{document.title}</p>
-                    ))}
+                    <DocumentList />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
