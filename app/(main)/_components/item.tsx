e3592fb47acd6ce86960663f51a468477beb2747
João Pedro <joaopedrodev@hotmail.com>
2026-02-03 08:23:18 -0300

    feat: add archive and skeleton item in navigator

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Kbd } from "@/components/ui/kbd";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
-import { ChevronDownIcon, ChevronRightIcon, LucideIcon, PlusIcon } from "lucide-react";
+import { ChevronDownIcon, ChevronRightIcon, LucideIcon, MoreHorizontal, MoreHorizontalIcon, PlusIcon, Trash2Icon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useUser } from "@clerk/clerk-react";
 
 interface ItemProps {
     id?: Id<"documents">;
@@ -37,8 +39,23 @@ export const Item = ({
     icon: Icon
 }: ItemProps) => {
 
+    const { user } = useUser();
     const router = useRouter();
     const create = useMutation(api.documents.create);
+    const archive = useMutation(api.documents.archive);
+
+    const onArchive = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        if (!id) return;
+
+        const promise = archive({ id });
+
+        toast.promise(promise, {
+            loading: "Movendo documento para a lixeira...",
+            success: "Documento movido para a lixeira",
+            error: "Erro ao mover documento para a lixeira!",
+        });
+    }
 
     const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.stopPropagation();
@@ -53,12 +70,12 @@ export const Item = ({
             title: "Untitled",
             parentDocument: id
         })
-        .then((documentId) => {
-            if (!expanded) {
-                onExpand?.();
-            }
-            // router.push(`/documents/${documentId}`);
-        });
+            .then((documentId) => {
+                if (!expanded) {
+                    onExpand?.();
+                }
+                // router.push(`/documents/${documentId}`);
+            });
 
         toast.promise(promise, {
             loading: "Criando documento...",
@@ -102,8 +119,39 @@ export const Item = ({
             )}
             {!!id && (
                 <div className="ml-auto flex items-center gap-x-2">
-                    <div role="button" onClick={onCreate} className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
-                        <PlusIcon className="size-4 text-muted-foreground"/>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger
+                            onCanPlay={(e) => { e.stopPropagation() }}
+                            asChild
+                        >
+                            <Button variant={"ghost"} className="opacity-0 group-hover:opacity-100 ml-auto size-6">
+                                <MoreHorizontalIcon className="text-muted-foreground"/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className="w-60"
+                            align="start"
+                            side="right"
+                            forceMount
+                        >
+                            <DropdownMenuItem onClick={onArchive}>
+                                <Trash2Icon className=""/>
+                                Apagar
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <div className="text-xs text-muted-foreground p-2">
+                                Editado por: {user?.fullName}
+                            </div>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    <div 
+                        onClick={onCreate}
+                        className={cn(
+                            buttonVariants({ variant: "ghost" }),
+                            "opacity-0 group-hover:opacity-100 ml-auto size-6",
+                        )}>
+                        <PlusIcon className="text-muted-foreground"/>
                     </div>
                 </div>
             )}
@@ -119,8 +167,8 @@ Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
             }}
             className="flex gap-2 py-[3px]"
         >
-            <Skeleton className="size-4"/>
-            <Skeleton className="h-4 w-[30%]"/>
+            <Skeleton className="size-4" />
+            <Skeleton className="h-4 w-[30%]" />
         </div>
     )
 }
