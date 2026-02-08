5bdd8f9e89fd9d94c991a502d638c0f3ab0ac218
João Pedro <joaopedrodev@hotmail.com>
2026-02-08 10:21:46 -0300

    feat: add toolbar component in page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,41 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { use } from "react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+    params: Promise<{
+        documentId: Id<"documents">;
+    }>;
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+    const { documentId } = use(params);
+
+    const document = useQuery(api.documents.getById, {
+        documentId: documentId,
+    })
+
+    if (document === undefined) {
+        return (
+            <p>Carregando...</p>
+        );
+    }
+
+    if (document === null) {
+        return (
+            <div>Não encontrado</div>
+        );
+    }
     return (
-        <div>
-            <h1>DocumentIdPage</h1>
+        <div className="pb-40">
+            <div className="h-[35vh]" />
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document}/>
+            </div>
         </div>
     );
 };
